c9aa184ed660c50ac9bbd6ce0c7c6effe466393a
iamyourdre <iamyourdre@gmail.com>
2025-02-10 11:45:33 +0700

    add Campaign page and CampaignList component; update App routing and create reusable table components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Base from './layout/Base';
 import Index from './pages/Index';
+import Campaign from './pages/Campaign';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Base>
         <Routes>
           <Route path="/" element={<Index/>} />
+          <Route path="/campaign" element={<Campaign/>} />
         </Routes>
       </Base>
     </Router>
--- frontend/src/pages/Campaign.jsx
+++ frontend/src/pages/Campaign.jsx
@@ -1,8 +1,17 @@
+import CampaignList from '@/components/CampaignList'
 import React from 'react'
 
 const Campaign = () => {
   return (
-    <div>Campaign</div>
+    <div className='box pt-28 flex'>
+      <div className="flex-1 flex flex-col gap-5">
+        <div>
+          <h1 className="text-3xl font-bold">Campaign List</h1>
+          <p className="text-sm text-gray-500 mt-2">A list of last 10 campaign.</p>
+        </div>
+        <CampaignList />
+      </div>
+    </div>
   )
 }
 
